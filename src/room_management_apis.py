39c7851b36f57ffae2965e4c915f4ca48d498fcc
Salil Vanvari <salilvanvari@berkeley.edu>
2020-05-09 18:42:33 -0700

    test Heroku Deploy

--- src/room_management_apis.py
+++ src/room_management_apis.py
@@ -3,4 +3,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return 'BRO CHAT EXISTS'
+
+if __name__ == '__main__':
+    # Threaded option to enable multiple instances for multiple user access support
+    app.run(threaded=True, port=5000)
